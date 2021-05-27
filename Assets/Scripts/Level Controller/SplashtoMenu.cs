6f8a446f1bd468fd812ebe3c498ca05878fcdfc8
Gopi Chand <gopichandde@gmail.com>
2021-05-27 17:22:13 +0530

    added the logo animation and script

---
+++ Assets/Scripts/Level Controller/SplashtoMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SplashtoMenu : MonoBehaviour
+{
+    [SerializeField] string menu;
+    // Start is called before the first frame update
+   
+    
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Timer());
+
+        
+    }
+    IEnumerator Timer(){
+        yield return new WaitForSeconds(5f);
+        GoNextLevel();
+    }
+    void GoNextLevel()
+    {
+        SceneManager.LoadSceneAsync(menu);
+
+    }
+}
