211f8687bf5e342bdc4e030d2ef3aeb1dad9ace3
Gopi Chand <gopichandde@gmail.com>
2021-06-07 22:20:41 +0530

    added the setting and back button

--- Assets/Scripts/Level Controller/LevelController.cs
+++ Assets/Scripts/Level Controller/LevelController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TaptoPlay : MonoBehaviour
+public class LevelController : MonoBehaviour
 {
     [SerializeField] string ListofWorld;
     Touch touch;
