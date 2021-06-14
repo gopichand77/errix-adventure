e469fac0d49adb8c5136f3a05678b3258af0710c
Gopi Chand <gopichandde@gmail.com>
2021-06-14 22:10:12 +0530

    done with movement script and animation and added the dust effect

---
+++ Assets/Scripts/Player/Parallax.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    private float length, startPos;
+    public GameObject cam;
+    public float parallaxEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float repeat = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+
+        if(repeat > startPos + length) {
+            startPos += length;
+        } 
+        else if(repeat < startPos - length) {
+            startPos -= length;
+        }
+    }
+}
