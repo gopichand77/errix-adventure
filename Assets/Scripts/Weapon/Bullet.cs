12efbabb3d800d294aec5ed75059051e1c40ec28
Gopi Chand <gopichandde@gmail.com>
2021-06-17 14:38:39 +0530

    added the Sorting Order and Layer

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -6,33 +6,39 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 5f;
     public Rigidbody2D rb;
-   public  bool Death;
-   
-     public Animator Axe;
-   
+    public bool Death;
 
-   
-   
+    public Animator Axe;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
- }
- 
+    }
+    void Update(){
+        StartCoroutine(BulletDie());
+    }
+    IEnumerator BulletDie() {
+        yield return new WaitForSeconds(8);
+        Destroy(gameObject);
+    }
     // void OnTriggerEnter2D(Collider2D hitInfo)
     // {
     //     // Monster enemy = hitInfo.GetComponent<Monster>();
     //     // if(enemy !=null)
     //     // {
     //     //     enemy.TakeDamage(5);
-           
+
     //     //     }
 
-       
-        
+
+
 
     // }
-  
+
     // Update is called once per frame
-   
+
 }
