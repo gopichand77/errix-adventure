50bf7c1b29f451a0e69e63f2b8e9512930dcaf81
AVINASH3735 <avinashlakshmi7@gmail.com>
2021-07-21 13:51:07 +0530

    Fade Out

--- Assets/Scripts/Tile Platforms/UndergroundVisible.cs
+++ Assets/Scripts/Tile Platforms/UndergroundVisible.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class UndergroundVisible : MonoBehaviour
 {
+    [SerializeField]
+    Tilemap tilemap;
+    private float alphaValue;
+    
     // Start is called before the first frame update
     void Start()
     {
+        tilemap = gameObject.GetComponent<Tilemap>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if(alphaValue < 0.4)
+        // {
+        //     CancelInvoke("IncreaseAlpha");
+        // }
+       
+            
+        
         
     }
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("Player"))
+        {
+            tilemap.color = new  Color(1f, 1f, 1f, 0.4f);
+            // InvokeRepeating("IncreaseAlpha",0,0.03f);
+
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D trig)
+    {
+         if(trig.gameObject.CompareTag("Player"))
+        {
+            tilemap.color = new  Color(1f, 1f, 1f, 1f);
+            // InvokeRepeating("IncreaseAlpha",0,0.03f);
+
+
+        }
+    }
+    // void IncreaseAlpha()
+    // {
+    //     alphaValue -= 0.001f;
+
+    // }
 }
