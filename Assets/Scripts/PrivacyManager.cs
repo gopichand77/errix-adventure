d18c7366500fb8f8450a6c5e91f67856e9decdf7
Gopi Chand <gopichandde@gmail.com>
2021-08-19 15:39:14 +0530

    Added the Privacy Policy Menu

---
+++ Assets/Scripts/PrivacyManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrivacyManager : MonoBehaviour
+{
+
+    private string policykey = "policy";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var accepted = PlayerPrefs.GetInt(policykey, 0) == 1;
+
+        if(accepted)
+            return;
+        
+        SimpleGDPR.ShowDialog(new TermsOfServiceDialog().
+        SetPrivacyPolicyLink("https://errix.co/privacypolicy.html").
+        SetTermsOfServiceLink("https://errix.co/termsofservice.html"),
+        onMenuClosed
+        );
+
+    }
+
+    private void onMenuClosed(){
+        Debug.Log("PrivacyPolicy Accepted");
+        PlayerPrefs.SetInt(policykey, 1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
