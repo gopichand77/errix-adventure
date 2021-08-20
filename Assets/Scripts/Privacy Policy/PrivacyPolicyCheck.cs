b24268824dcf3da529b8ee49650bf46c41345456
Gopi Chand <gopichandde@gmail.com>
2021-08-20 22:06:36 +0530

    Updated the Dependences and Privacy Panel

--- Assets/Scripts/Privacy Policy/PrivacyPolicyCheck.cs
+++ Assets/Scripts/Privacy Policy/PrivacyPolicyCheck.cs
@@ -6,17 +6,20 @@ public class PrivacyPolicyCheck : MonoBehaviour
 {
 
     private string policykey = "policy";
+    private bool accepted;
     [SerializeField]
     GameObject privacyPanel;
     // Start is called before the first frame update
     void Start()
     {
-        var accepted = PlayerPrefs.GetInt(policykey, 0) == 1;
+        accepted = PlayerPrefs.GetInt(policykey, 0) == 1;
+        Debug.Log(accepted);
 
-        if(accepted)
+        if(!accepted)
             return;
         off();
         onMenuClosed();
+        
     }
 
     private void off(){
@@ -27,6 +30,21 @@ public class PrivacyPolicyCheck : MonoBehaviour
         Debug.Log("PrivacyPolicy Accepted");
         PlayerPrefs.SetInt(policykey, 1);
     }
+    public void setPref()
+    {
+      PlayerPrefs.SetInt(policykey, 1);
+      off();
+        onMenuClosed();
+      Debug.Log(accepted);
+
+    }
+    //  public void BackPref()
+    // {
+    //   PlayerPrefs.SetInt(policykey, 0);
+    //   Debug.Log(accepted);
+
+    // }
+
 
     // Update is called once per frame
     void Update()
