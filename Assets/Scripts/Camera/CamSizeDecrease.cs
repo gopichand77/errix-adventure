2e65dc0e543f719e85737837d82048dd744b5f57
AVINASH3735 <53428833+AVINASH3735@users.noreply.github.com>
2021-09-16 23:46:00 +0530

    Camera Size Done

--- Assets/Scripts/Camera/CamSizeDecrease.cs
+++ Assets/Scripts/Camera/CamSizeDecrease.cs
@@ -5,51 +5,93 @@ using UnityEngine;
 public class CamSizeDecrease : MonoBehaviour
 {
     // Start is called before the first frame update
-        public float size = 6;
-    private float defaultSize = 5;
-    [SerializeField]
-    Camera cam;
+   [SerializeField] float endOfZoomInCursor = .001f; 
+    [SerializeField] float endOfZoomOutCursor = .05f;
 
-    private void Update()
-    {
-        if(cam.orthographicSize <  defaultSize )
-            {
-                CancelInvoke("DecreaseCam");
-           
-            }
-            // if(cam.orthographicSize < defaultSize)
-            // {
-            //     CancelInvoke("DecreaseCam");
-           
-            // }
-    }
+    //Target values for the zoom effect
+    [SerializeField] float zoomInSpeed = 1f;
+    [SerializeField] float zoomOutSpeed = 1f;
+    [SerializeField] float targetZoomIn = .3f;
+    [SerializeField] float targetZoomOut = .3f;
+
+    //Initial value for zoom out effect
+    float initalZoom = 0f;// I am just going to keep this typo after all :)
+    float initialPosX;
+    float initialPosY;
+
+    //State bool for controling the methods
+    public static bool IsZoomedIn = false;
+    public static bool IsZoomedOut = false;
+
+    //Cache reference
+    public Transform target;
+    public Camera cam;
+    public bool zoomin;
+    public bool zoomout;
+    public bool zoom;
+
+
+
+
+
     // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D collider2D){
-        if(collider2D.gameObject.CompareTag("Player")){
-            // cam.orthographicSize += 10 * Time.deltaTime;
-            InvokeRepeating("DecreaseCam",0,0.03f);
+    //Initializing our values
+    void Start()
+    {
+        cam = Camera.main;
+        initalZoom = cam.orthographicSize;
+        initialPosX = cam.transform.position.x;
+        initialPosY = cam.transform.position.y;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.CompareTag("Player"))
+        {
+            ZoomInCamera();
         }
     }
 
-    // private void OnTriggerStay2D(Collider2D collider2D) {
-    //     if(collider2D.gameObject.CompareTag("Player")){
-    //         // cam.orthographicSize += 10 * Time.deltaTime;
-    //         InvokeRepeating("IncreaseCam",0,0.03f);
-    //     }
-    // }
-    
-    // private void OnTriggerExit2D(Collider2D trig)
-    // {
-    //       InvokeRepeating("DecreaseCam",0,0.03f);
-        
-    // }
-    void IncreaseCam()
+    public void ZoomInCamera()
     {
-        cam.orthographicSize += 0.01f;
+        if (!IsZoomedIn)
+        {
+            cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, initalZoom, Time.deltaTime * zoomInSpeed);
+            cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, target.position.x, Time.deltaTime * zoomInSpeed),
+                Mathf.Lerp(cam.transform.position.y,cam.transform.position.y , Time.deltaTime * zoomInSpeed), 
+                cam.transform.position.z);
+            if (initalZoom <= endOfZoomInCursor)
+            {
+                IsZoomedIn = true;
+            }
+        }
     }
-     void DecreaseCam()
-    {
-        cam.orthographicSize -= 0.01f;
 
+    public void ZoomOutCamera()
+    {
+
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoomOut, Time.deltaTime * zoomInSpeed);
+            cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, target.position.x, Time.deltaTime * zoomInSpeed),
+                Mathf.Lerp(cam.transform.position.y, cam.transform.position.y, Time.deltaTime * zoomInSpeed), 
+                cam.transform.position.z);
+            if (initalZoom + targetZoomOut <= endOfZoomOutCursor)
+            {
+                IsZoomedIn = true;
+            }
+        // if (IsZoomedIn)
+        // {
+        //     cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, initalZoom, Time.deltaTime * zoomOutSpeed);
+        //     cam.transform.position = new Vector3(Mathf.Lerp(cam.transform.position.x, initialPosX, Time.deltaTime * zoomOutSpeed),
+        //         Mathf.Lerp(cam.transform.position.y, initialPosY, Time.deltaTime * zoomOutSpeed),
+        //         cam.transform.position.z);
+        //     if (initalZoom - cam.orthographicSize <= endOfZoomOutCursor)
+        //     {
+        //         IsZoomedIn = false;
+        //         IsZoomedOut = true;
+        //         cam.orthographicSize = initalZoom;
+        //         cam.transform.position = new Vector3(initialPosX, initialPosY, cam.transform.position.z);
+        //     }
+        // }
     }
+
 }
