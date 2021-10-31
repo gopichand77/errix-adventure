9b3f517f7706f5e0dcb4301e3e9afa7f14d02247
AVINASH3735 <53428833+AVINASH3735@users.noreply.github.com>
2021-10-31 14:31:30 +0530

    play games update

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "901054236957"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "901054236957-2pko8g0dqnj928rvq08khnqom1c2ho0j.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
