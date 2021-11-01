4b15674fd46ba1aab2f72f4893c88f107cc7ded1
Gopi Chand <gopichandde@gmail.com>
2021-11-01 15:37:55 +0530

    added the intro music

---
+++ Assets/Scripts/ScreenShot.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScreenShot : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ScreenCapture.CaptureScreenshot("/Users/gopichand/Desktop/Screenshots-test/img.png");
+        }
+    }
+}
