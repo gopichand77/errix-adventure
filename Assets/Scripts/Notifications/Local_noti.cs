a852741cc24fb41984bd36cbcb40e1a327f639aa
Gopi Chand <gopichandde@gmail.com>
2022-02-04 21:43:29 +0530

    added enemies and local notifications package

---
+++ Assets/Scripts/Notifications/Local_noti.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_ANDROID
+using Unity.Notifications.Android;
+#elif UNITY_IOS
+using Unity.Notifications.iOS;
+#endif
+public class Local_noti : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
