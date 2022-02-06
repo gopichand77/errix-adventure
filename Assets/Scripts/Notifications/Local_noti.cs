c642ce37a123a402ca4912df4cb037f70565c5c2
AVINASH3735 <53428833+AVINASH3735@users.noreply.github.com>
2022-02-06 11:30:11 +0530

    added local notif

--- Assets/Scripts/Notifications/Local_noti.cs
+++ Assets/Scripts/Notifications/Local_noti.cs
@@ -1,19 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
-#if UNITY_ANDROID
-using Unity.Notifications.Android;
-#elif UNITY_IOS
+
+
+using System.Collections;
+#if UNITY_IOS
 using Unity.Notifications.iOS;
+#elif UNITY_ANDROID
+using Unity.Notifications.Android;
 #endif
 public class Local_noti : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+var channel = new AndroidNotificationChannel()
+{
+    Id = "channel_id",
+    Name = "Default Channel",
+    Importance = Importance.Default,
+    Description = "Generic notifications",
+};
+AndroidNotificationCenter.RegisterNotificationChannel(channel);
 
+AndroidNotification notification =  new AndroidNotification();
+{
+  var notification1 = new AndroidNotification();
+notification1.Title = "Your Title";
+notification1.Text = "Your Text";
+notification1.FireTime = System.DateTime.Now.AddMinutes(1);
+
+  
+};
+AndroidNotificationCenter.SendNotification(notification, "channel_id");
+    }
+    
     // Update is called once per frame
     void Update()
     {
