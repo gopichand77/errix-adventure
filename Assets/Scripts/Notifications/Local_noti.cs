d82363f2962a1f2878d659c70df0386bc90ba327
Gopi Chand <gopichandde@gmail.com>
2022-02-06 22:27:53 +0530

    added the credits scene

--- Assets/Scripts/Notifications/Local_noti.cs
+++ Assets/Scripts/Notifications/Local_noti.cs
@@ -1,7 +1,5 @@
 using System;
 using UnityEngine;
-
-
 using System.Collections;
 #if UNITY_IOS
 using Unity.Notifications.iOS;
@@ -12,28 +10,28 @@ public class Local_noti : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-var channel = new AndroidNotificationChannel()
-{
-    Id = "channel_id",
-    Name = "Default Channel",
-    Importance = Importance.Default,
-    Description = "Generic notifications",
-};
-AndroidNotificationCenter.RegisterNotificationChannel(channel);
+    {   
+        #if UNITY_ANDROID
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = "channel_id",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Generic notifications",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
 
-AndroidNotification notification =  new AndroidNotification();
-{
-  var notification1 = new AndroidNotification();
-notification1.Title = "Your Title";
-notification1.Text = "Your Text";
-notification1.FireTime = System.DateTime.Now.AddMinutes(1);
-
-  
-};
-AndroidNotificationCenter.SendNotification(notification, "channel_id");
+        AndroidNotification notification = new AndroidNotification();
+        {
+            var notification1 = new AndroidNotification();
+            notification1.Title = "Your Title";
+            notification1.Text = "Your Text";
+            notification1.FireTime = System.DateTime.Now.AddMinutes(1);
+        };
+        AndroidNotificationCenter.SendNotification(notification, "channel_id");
+        #endif
     }
-    
+
     // Update is called once per frame
     void Update()
     {
