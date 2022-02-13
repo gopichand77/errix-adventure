111ec84575b76779787b95416ffc8c3038227c32
AVINASH3735 <53428833+AVINASH3735@users.noreply.github.com>
2022-02-13 11:43:31 +0530

    Changed Prefab

--- Assets/Scripts/Notifications/Local_noti.cs
+++ Assets/Scripts/Notifications/Local_noti.cs
@@ -1,40 +1,64 @@
 using System;
 using UnityEngine;
 using System.Collections;
-#if UNITY_IOS
-using Unity.Notifications.iOS;
-#elif UNITY_ANDROID
-using Unity.Notifications.Android;
-#endif
+// #if UNITY_IOS
+// using Unity.Notifications.iOS;
+// #elif UNITY_ANDROID
+using Assets.SimpleAndroidNotifications;
+// #endif
 public class Local_noti : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {   
-        #if UNITY_ANDROID
-        var channel = new AndroidNotificationChannel()
-        {
-            Id = "channel_id",
-            Name = "Default Channel",
-            Importance = Importance.Default,
-            Description = "Generic notifications",
-        };
-        AndroidNotificationCenter.RegisterNotificationChannel(channel);
-
-        AndroidNotification notification = new AndroidNotification();
-        {
-            var notification1 = new AndroidNotification();
-            notification1.Title = "Your Title";
-            notification1.Text = "Your Text";
-            notification1.FireTime = System.DateTime.Now.AddMinutes(1);
-        };
-        AndroidNotificationCenter.SendNotification(notification, "channel_id");
-        #endif
+   
     }
-
     // Update is called once per frame
     void Update()
     {
 
     }
+    // public void Click()
+    // {
+    //          #if UNITY_ANDROID
+    //     var channel = new AndroidNotificationChannel()
+    //     {
+    //         Id = "channel_id",
+    //         Name = "Default Channel",
+    //         Importance = Importance.Default,
+    //         Description = "Generic notifications",
+    //     };
+    //     AndroidNotificationCenter.RegisterNotificationChannel(channel);
+
+    //     // AndroidNotification notification = new AndroidNotification();
+    //     // {
+    //         var notification = new AndroidNotification();
+    //         notification.Title = "Your Title";
+    //         notification.Text = "Your Text";
+    //         notification.FireTime = System.DateTime.Now.AddMinutes(1);
+    //     // };
+    //     AndroidNotificationCenter.SendNotification(notification, "channel_id");
+    //     #endif
+    // }
+     public void ScheduleCustom()
+        {
+            var notificationParams = new NotificationParams
+            {
+                Id = UnityEngine.Random.Range(0, int.MaxValue),
+                Delay = TimeSpan.FromSeconds(5),
+                Title = "Custom notification",
+                Message = "Message",
+                Ticker = "Ticker",
+                Sound = true,
+                Vibrate = true,
+                Light = true,
+                SmallIcon = NotificationIcon.Heart,
+                SmallIconColor = new Color(0, 0.5f, 0),
+                LargeIcon = "app_icon"
+            };
+
+            NotificationManager.SendCustom(notificationParams);
+        }
+
+    
 }
