7177c1a91492fdd463eb88f655695c5804f4e488
Gopi Chand <gopichandde@gmail.com>
2022-02-28 15:53:22 +0530

    repair 👩‍🔧 🧑‍🔧 👨‍🔧

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -49,11 +49,11 @@ public class PlayerMovement : MonoBehaviour
     void Movement()
     {
     #if UNITY_EDITOR
-    // dirX = Input.GetAxis("Horizontal") * moveSpeed;
+    dirX = Input.GetAxis("Horizontal") * moveSpeed;
     // // dirY = Input.GetKeyDown(KeyCode.W);
-    // dirY = Input.GetButtonDown("Jump");
-    dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
-    dirY = CrossPlatformInputManager.GetButtonDown("Jump");
+    dirY = Input.GetButtonDown("Jump");
+    // dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+    // dirY = CrossPlatformInputManager.GetButtonDown("Jump");
     #elif UNITY_ANDROID
     dirX = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
     dirY = CrossPlatformInputManager.GetButtonDown("Jump");
