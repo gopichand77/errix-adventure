ee6687660726200bca855a31dfc566605ded1905
AVINASH3735 <53428833+AVINASH3735@users.noreply.github.com>
2022-03-03 16:46:39 +0530

    Camera adjusted

--- Assets/Scripts/Camera/BossLevel.cs
+++ Assets/Scripts/Camera/BossLevel.cs
@@ -18,54 +18,69 @@ public class BossLevel : MonoBehaviour
     internal Player playerScript;
     [SerializeField]
     CameraController Camera;
-    public int timer;
+    public float timer;
     public int stayTimer;
     private void Start()
     {
+        timer = 6;
+        // EnemyTransform = FindObjectOfType<Enemy_Behaviour>().transform;
+        move_ = true;
         cam = gameObject;
         Camera = GetComponent<CameraController>();
-
         playerScript =  FindObjectOfType<Player>();
         PlayerTransform =  playerScript.transform;
         Camera.enabled = false;
         playerScript.MovementScript.ctrlActive = false;
-        move_ = false;
-        StartCoroutine(Move());
+        move();
+        
         // PlayerTransform = 
     }
 
     void Update()
     {
-        if (move_)
+        if(move_)
         {
-            //Assigning new position to moveTOPosition
-            moveToPosition = new Vector3(EnemyTransform.position.x, EnemyTransform.position.y, -10);
-            cam.transform.position =
-            Vector3.SmoothDamp(cam.transform.position,
-                          moveToPosition,
-                          ref matric, speed);
-
-
-
+            move();
+            timer -= Time.deltaTime;
 
         }
-        if (move_back)
+        if(timer < 0)
         {
-            moveToPosition = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, -10);
-            cam.transform.position =
-            Vector3.SmoothDamp(cam.transform.position,
-                          moveToPosition,
-                          ref matric, speed);
-
+            move_ = false;
+            Camera.enabled = true;
         }
+         
     }
 
     // public void move()
     // {
     //     move_ = true;
     // }
+    public void move()
+    {
+         moveToPosition = new Vector3(EnemyTransform.position.x, EnemyTransform.position.y, -10);
+            cam.transform.position =
+            Vector3.SmoothDamp(cam.transform.position,
+                          moveToPosition,
+                          ref matric, speed);
+                          
+            StartCoroutine(Move());
+
+    }
+    public void Back()
+    {
+        moveToPosition = new Vector3(PlayerTransform.position.x, PlayerTransform.position.y, -10);
+            cam.transform.position =
+            Vector3.SmoothDamp(cam.transform.position,
+                          moveToPosition,
+                          ref matric, speed);
+        //  Camera.enabled = true;
+
+    }
     IEnumerator Move()
     {
+        if(move_back)
+        {
         yield return new WaitForSeconds(2f);
         move_ = true;
         yield return new WaitForSeconds(timer);
@@ -78,7 +93,7 @@ public class BossLevel : MonoBehaviour
         Camera.enabled = true;
         playerScript.MovementScript.ctrlActive = true;
 
-
+        }
 
 
     }
